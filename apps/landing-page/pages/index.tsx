f38edf4775ec451bc10dd770fe9b2c5a400736fe
Joel Hooks <joelhooks@gmail.com>
2022-08-13 13:59:27 -0700

    feat: add a next app and a react component package
    the component package doesn't have a build step and the components are directly imported. this means that we need to tell next to transpile the imported components!

---
+++ apps/landing-page/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import {Hello} from 'react-ui'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          <Hello />
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
